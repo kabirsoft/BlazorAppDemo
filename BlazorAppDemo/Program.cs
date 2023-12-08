33f881b68a281c3af1306d1f97ce62b065ba8bf6
mka198-cloud <mka198@uit.no>
2023-12-08 15:38:29 +0100

    added dummy key for api

--- BlazorAppDemo/Program.cs
+++ BlazorAppDemo/Program.cs
@@ -7,7 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["api_base_url"]) });
 builder.Services.AddHttpClient<IFetchUni, FetchUni>(sp => sp.BaseAddress = new Uri(builder.Configuration["api_base_url"]));
 builder.Services.AddHttpClient<ICountryData, CountryData>(sp => sp.BaseAddress = new Uri(builder.Configuration["country_graphQL_url"]));
--- BlazorAppDemo/Services/FetchUni.cs
+++ BlazorAppDemo/Services/FetchUni.cs
@@ -1,15 +1,21 @@
 ﻿using BlazorAppDemo.Dtos;
 using System.Net.Http.Json;
+using Microsoft.Extensions.Configuration;
 
 namespace BlazorAppDemo.Services
 {
     public class FetchUni : IFetchUni
     {
         private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+        private readonly string _apiKey;
 
-        public FetchUni(HttpClient httpClient)
+        public FetchUni(HttpClient httpClient, IConfiguration configuration)
         {
             _httpClient = httpClient;
+            _configuration = configuration;
+            _apiKey = _configuration["ApiKey"];
+            _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _apiKey);
         }        
 
         public async Task<UniversityDto[]> GetAll()
